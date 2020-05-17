4ca76a7fa853db4f591a328a1b9172a5188ceb39
Rashadat Mirzayev <334rashad@gmail.com>
2020-05-17 23:56:46 +0200

    destroying enemy objects

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [Range(0.5f, 15f)] [SerializeField] float shootSpeed = 3f;
+    [SerializeField] float damage = 50f;
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector2.right * shootSpeed * Time.deltaTime);
     }
 
-    private void OnTriggerEnter2D(Collider2D projectileCollider)
+    private void OnTriggerEnter2D(Collider2D enemyCollider)
     {
-        Debug.Log("Shooting at " + projectileCollider.name);
+        var health = enemyCollider.GetComponent<Health>();
+        health.DealDamage(damage);
     }
 }
