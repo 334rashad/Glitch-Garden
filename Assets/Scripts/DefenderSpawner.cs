a2be94452529fb05fb10830a0860a9511b744549
Rashadat Mirzayev <334rashad@gmail.com>
2020-05-19 00:12:57 +0200

    snap defender to grid

--- Assets/Scripts/DefenderSpawner.cs
+++ Assets/Scripts/DefenderSpawner.cs
@@ -14,7 +14,8 @@ public class DefenderSpawner : MonoBehaviour
     {
         Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
         Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
-        return worldPos;
+        Vector2 gridPos = SnapToGrid(worldPos);
+        return gridPos;
     }
 
     private Vector2 SnapToGrid(Vector2 rawWorldPos)
@@ -25,11 +26,10 @@ public class DefenderSpawner : MonoBehaviour
     }
 
 
-    private void SpawnDefender(Vector2 worldPos)
+    private void SpawnDefender(Vector2 roundedPos)
     {
-
         GameObject newDefender = Instantiate(defenderType,
-            worldPos, 
+            roundedPos, 
             Quaternion.identity) as GameObject;
     }
 }
