5b2e608192f9ea1e1c517c7d9d5151ce976829f6
Rashadat Mirzayev <334rashad@gmail.com>
2020-05-21 23:57:23 +0200

    add more coins spend coins

---
+++ Assets/Scripts/CoinsDisplay.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinsDisplay : MonoBehaviour
+{
+    [SerializeField] int coins = 100;
+    Text coinsText;
+    private void Start()
+    {
+        coinsText = GetComponent<Text>();
+        UpdateDisplay();   
+    }
+
+    private void UpdateDisplay()
+    {
+        coinsText.text = coins.ToString();
+    }
+    
+    public void AddCoins(int moreCoins)
+    {
+        coins += moreCoins;
+        UpdateDisplay();
+    }
+
+    public void SpendCoins(int moreCoins)
+    {
+        if (coins >= moreCoins)
+        {
+            coins -= moreCoins;
+            UpdateDisplay();
+        }
+    }
+}
