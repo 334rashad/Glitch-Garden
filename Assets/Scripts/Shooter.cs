bf76aa2dcc4ac64c99fb9ae11c6c96678e0e9711
Rashadat Mirzayev <334rashad@gmail.com>
2020-05-25 23:59:44 +0200

    detect attacker in the lane

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -1,11 +1,37 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
-
     [SerializeField] GameObject projectile, gun;
+
+    private void Start()
+    {
+        SetLaneSpawner();
+    }
+
+    private void Update()
+    {
+        if (AttackerIsComing())
+        {
+            Debug.Log("Shoot! \n");
+        } else
+        {
+            Debug.Log("Position clean");
+        }
+    }
+
+    private void SetLaneSpawner()
+    {
+        throw new NotImplementedException();
+    }
+
+    private bool AttackerIsComing()
+    {
+        throw new NotImplementedException();
+    }
 
     public void Fire()
     {
