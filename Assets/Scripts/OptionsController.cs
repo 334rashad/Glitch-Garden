ba808635e0932c07707fabfa9b9220e5cfcd6ace
Rashadat Mirzayev <334rashad@gmail.com>
2020-06-18 02:10:25 +0200

    Set music volume save volume, default volume

---
+++ Assets/Scripts/OptionsController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsController : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+    [SerializeField] float defaultVolume = 0.7f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        volumeSlider.value = PlayerPrefController.GetMasterVolume();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var musicPlayer = FindObjectOfType<MusicPlayer>();
+        if (musicPlayer)
+        {
+            musicPlayer.SetVolume(volumeSlider.value);
+        }
+        else Debug.Log("No Music Player Found");
+    }
+
+    public void SaveAndExit()
+    {
+        PlayerPrefController.SetMasterVolume(volumeSlider.value);
+        FindObjectOfType<LevelLoader>().LoadMainMenu();
+    }
+
+    public void SetDefaultVolume()
+    {
+        volumeSlider.value = defaultVolume;
+    }
+}
