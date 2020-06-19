25d406530d3f08c56497ec0e7ff945cabdbf702d
Rashadat Mirzayev <334rashad@gmail.com>
2020-06-19 23:59:47 +0200

    bug fixing for win condition

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -55,4 +55,4 @@ public class LevelLoader : MonoBehaviour
         Application.Quit();
     }
 } 
-  
+   
--- Assets/Scripts/OptionsController.cs
+++ Assets/Scripts/OptionsController.cs
@@ -41,4 +41,4 @@ public class OptionsController : MonoBehaviour
         diffulctySlider.value = defaultDiffulcty; 
     }
 }
- 
+  
