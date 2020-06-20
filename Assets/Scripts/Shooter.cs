3e9dc0791cedc1113228b0b189ce1cefbf919e13
Rashadat Mirzayev <334rashad@gmail.com>
2020-06-21 00:35:58 +0200

    creating parend gameobjects for defenders and projectiles

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -8,11 +8,23 @@ public class Shooter : MonoBehaviour
     [SerializeField] GameObject projectile, gun;
     AttackerSpawner myLaneSpawner;
     Animator animator;
+    GameObject projectileParent;
+    const string PROJECTILE_PARENT_NAME = "Projectiles";
+
+    private void CreateProjectileParent()
+    {
+        projectileParent = GameObject.Find(PROJECTILE_PARENT_NAME);
+        if (!projectileParent)
+        {
+            projectileParent = new GameObject(PROJECTILE_PARENT_NAME);
+        }
+    }
 
     private void Start()
     {
         SetLaneSpawner();
         animator = GetComponent<Animator>();
+        CreateProjectileParent();
     }
 
     private void Update()
@@ -48,6 +60,10 @@ public class Shooter : MonoBehaviour
 
     public void Fire()
     {
-        Instantiate(projectile, gun.transform.position, transform.rotation);
+        GameObject newProjectile = Instantiate(projectile, 
+            gun.transform.position, 
+            transform.rotation) as GameObject;
+        newProjectile.transform.parent = projectileParent.transform;
+
     }
 }
